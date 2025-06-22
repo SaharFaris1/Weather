abb773fc24c2e15ea1996f7b5e9001317fba7d89
Sahar Faris <saharfaris@Sahars-Laptop.local>
2025-06-22 22:50:10 +0300

    first commit

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Signup from '../pages/Signup';
 import Signin from '../pages/Signin';
